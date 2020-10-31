c4ddc366a5a8f6ddc60ba9e62cb7320d5b599116
321zer0 <73757749+321zer0@users.noreply.github.com>
2020-10-31 23:33:28 +0400

    Add ToString() in VideoStream.cs

--- FFvid Editor Pro/FFvid Editor Pro/VideoStream.cs
+++ FFvid Editor Pro/FFvid Editor Pro/VideoStream.cs
@@ -25,5 +25,16 @@ namespace FFvid_Editor_Pro
         public string PixelFormat { get; private set; }
         
         public float FrameRate { get; private set; }
+
+        public override string ToString()
+        {
+            return "Stream Type: " + this.Type + "\n" +
+                   "Index: " + this.Index.ToString() + "\n" +
+                   "Codec: " + this.Codec + "\n" +
+                   "Pixel format: " + this.PixelFormat + "\n" +
+                   "Frame rate: " + this.FrameRate + " fps" + "\n" +
+                   "Resolution: " + this.Width.ToString() + "x" + this.Height.ToString() + "\n" +
+                   "Bitrate: " + this.Bitrate + " kbps";
+        }
     }
 }
